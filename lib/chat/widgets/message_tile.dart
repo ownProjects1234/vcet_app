ab6168cf2019adb90726cceae80e3992fc094478
RaMpRaSaTh <ramprasath21022003@gmail.com>
2022-01-17 15:46:18 +0530

    jan17

--- lib/chat/widgets/message_tile.dart
+++ lib/chat/widgets/message_tile.dart
@@ -5,43 +5,75 @@ class MessageTile extends StatelessWidget {
   final String sender;
   final bool sentByMe;
 
-  const MessageTile({Key? key, required this.message, required this.sender, required this.sentByMe}) : super(key: key);
+  const MessageTile(
+      {Key? key,
+      required this.message,
+      required this.sender,
+      required this.sentByMe})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-  
       padding: EdgeInsets.only(
         top: 4,
         bottom: 4,
         left: sentByMe ? 0 : 24,
         right: sentByMe ? 24 : 0,
       ),
-      alignment:  sentByMe ? Alignment.centerRight : Alignment.centerLeft,
+      alignment: sentByMe ? Alignment.centerRight : Alignment.centerLeft,
       child: Container(
-        margin: sentByMe ? const EdgeInsets.only(left: 30) : EdgeInsets.only(right: 30),
-        padding: const EdgeInsets.only(top: 17, bottom:  17, left: 20, right: 20),
+        margin: sentByMe
+            ? const EdgeInsets.only(left: 30)
+            : EdgeInsets.only(right: 30),
+        padding:
+            const EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
         decoration: BoxDecoration(
-          color: Colors.black87,
-          borderRadius: sentByMe ? const BorderRadius.only(
-          topLeft:  Radius.circular(23),
-          topRight: Radius.circular(23),
-          bottomLeft: Radius.circular(23)
-        )
-        :
-        const BorderRadius.only(
-          topLeft: Radius.circular(23),
-          topRight: Radius.circular(23),
-          bottomRight: Radius.circular(23)
-        )
-        ),
-
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(
+                  4.0,
+                  4.0,
+                ),
+                blurRadius: 10.0,
+                spreadRadius: 1.0,
+              ), //BoxShadow
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0.0, 0.0),
+                blurRadius: 0.0,
+                spreadRadius: 0.0,
+              ), //BoxShadow
+            ],
+            color: Colors.black87,
+            borderRadius: sentByMe
+                ? const BorderRadius.only(
+                    topLeft: Radius.circular(23),
+                    topRight: Radius.circular(23),
+                    bottomLeft: Radius.circular(23))
+                : const BorderRadius.only(
+                    topLeft: Radius.circular(23),
+                    topRight: Radius.circular(23),
+                    bottomRight: Radius.circular(23))),
         child: Column(
-          crossAxisAlignment:  CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(sender.toUpperCase(), textAlign: TextAlign.start, style: const TextStyle(fontSize: 13.0, color:  Colors.white, letterSpacing: -0.5)),
-            const SizedBox(height:  7.0,),
-            Text(message, textAlign: TextAlign.start, style: const TextStyle(fontSize: 17.0, color: Colors.white, fontWeight: FontWeight.bold),)
+            Text(sender.toUpperCase(),
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                    fontSize: 13.0, color: Colors.white, letterSpacing: -0.5)),
+            const SizedBox(
+              height: 7.0,
+            ),
+            Text(
+              message,
+              textAlign: TextAlign.start,
+              style: const TextStyle(
+                  fontSize: 17.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            )
           ],
         ),
       ),
